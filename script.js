a4d50efda2fb099e6425f5a9b2fab88f2b6dbe7f
Amang2004 <amangupta.ans@gmail.com>
2025-04-16 22:46:04 +0530

    dino fix the js file

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus")
+    const cactus = document.getElementById("cactus");
 
-function jump() {
-    if (dino.classList != )
-    dino.classList.add("jump")
-    
-    setTimeout(function (){
-        dino.classList.remove("jump");
-    } 300);
+    function jump() {
+        if (!dino.classList.contains("jump")) {
+            dino.classList.add("jump");
+
+            setTimeout(function () {
+                dino.classList.remove("jump");
+            }, 300);
+        }
     }
 
+    document.addEventListener("keydown", function (event) {
+        jump();
+    });
 
+    let isAlive = setInterval(function () {
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("bottom"));
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-
-
-
-
-document.addEventListener("keydown", function (event){
-     jump();
-});
-
-let isAlive = setInterval(function{
-let dinotop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-console.log(dinotop);
-
-},10);
-    
-
-}
+        if (cactusLeft < 70 && cactusLeft > 0 && dinoTop < 40) {
+            alert("💥 Game Over!");
+            // Reload the game
+            window.location.reload();
+        }
+    }, 10);
